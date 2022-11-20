aff76a7c7053459880d1acbdb3f93f5cb90b01ce
nagm-star <nagmo90@gmail.com>
2022-11-20 19:21:24 +0200

    fix bugs

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -6,8 +6,6 @@ use App\Models\Post;
 use App\Models\Slide;
 use App\Models\Contact;
 use App\Models\Setting;
-use App\Models\Project;
-use App\Models\Service;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\OpenGraph;
 use Artesaos\SEOTools\Facades\TwitterCard;
